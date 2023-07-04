4910efe115913214fd480306bb0516daff896774
shyaminayesh <me@shyamin.com>
2023-07-04 14:28:16 +0530

    CPU Info provider

--- providers/cpuinfo.go
+++ providers/cpuinfo.go
@@ -2,17 +2,56 @@ package provider
 
 import (
 	"agent/config"
-	"fmt"
+	"agent/utils"
 	"log"
+
+	"github.com/prometheus/procfs"
 )
 
 type CPUInfo struct{}
+
+type (
+	Core struct {
+		ID        uint8    `json:"id"`
+		ModelName string   `json:"name"`
+		MHz       float64  `json:"mhz"`
+		Flags     []string `json:"flags"`
+	}
+	Processor struct {
+		Cores []Core `json:"cores"`
+	}
+)
 
 /**
 * Gather information required by the module and pass
 * them back to the main worker for submission
  */
-func (provider CPUInfo) Gather() {
+func (provider CPUInfo) Gather() (Processor, error) {
+
+	pfs, err := procfs.NewFS("/proc")
+	if err != nil {
+		return Processor{}, err
+	}
+
+	CPUs, err := pfs.CPUInfo()
+	if err != nil {
+		return Processor{}, err
+	}
+
+	var cores []Core
+	for _, CPU := range CPUs {
+		// fmt.Printf("INFO: %d\n", CPU.Processor)
+		cores = append(cores, Core{
+			ID:        uint8(CPU.Processor),
+			ModelName: CPU.ModelName,
+			MHz:       CPU.CPUMHz,
+			Flags:     CPU.Flags,
+		})
+	}
+
+	return Processor{
+		Cores: cores,
+	}, nil
 
 }
 
@@ -20,16 +59,22 @@ func (provider CPUInfo) Gather() {
 func (provider CPUInfo) Worker(cfg config.Config) {
 	log.Printf("[INFO] MODULE: cpuinfo")
 
+	processors, err := provider.Gather()
+	if err != nil {
+		log.Printf("[ERROR] %s", err)
+		return
+	}
+
 	/**
 	* Check if the module is due for new submission
 	* and then invoke the Gather function to gather
 	* relevent informaiton
 	 */
-	fmt.Println(cfg.Agent.ID)
 
-	/**
-	 * Feed all the information to the upstream opsel
-	 * server to process and store
-	 */
-	// http.Post(fmt.Sprintf("%s://%s/gather/", cfg.Server.Schema, cfg.Server.URI))
+	// Feeder
+	if err := utils.Feeder(cfg, "/processor", processors); err != nil {
+		log.Printf("[ERROR] %s", err)
+		return
+	}
+
 }
